9f5c7b374b94870e6b06df2e67edfbf6ecbc8474
Brett Zamir <brettz9@yahoo.com>
2019-09-29 09:49:35 +0800

    - Linting (ESLint): Ignore `coverage` folder - npm: Update devDeps - Testing: Begin indexeddb setup

--- test/main.test.js
+++ test/main.test.js
@@ -1,7 +1,12 @@
-import {importCSVToIndexedDB} from '../src/index.js';
+/* globals indexedDB */
+import setGlobalVars from 'indexeddbshim/src/node-UnicodeIdentifiers.js';
+import {importCSVToIndexedDB, importJSONToIndexedDB} from '../src/index.js';
 
 describe('Main tests', function () {
   it('API', function () {
+    setGlobalVars();
     expect(importCSVToIndexedDB).to.be.a('function');
+    expect(importJSONToIndexedDB).to.be.a('function');
+    expect(indexedDB).to.be.an('IDBFactory');
   });
 });
